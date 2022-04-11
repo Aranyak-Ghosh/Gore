81e3b8de6e51ce560ba8c8033d3da703a83ca72b
Aranyak Ghosh <59360618+unicornlab-aranyakg@users.noreply.github.com>
2022-04-11 11:53:50 +0400

    More test

--- RecoveryMiddleware_test.go
+++ RecoveryMiddleware_test.go
@@ -1,6 +1,7 @@
 package gore
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/gin-gonic/gin"
@@ -13,6 +14,10 @@ func TestMiddleware(t *testing.T) {
 
 	r.GET("test", func(ctx *gin.Context) {
 		panic("Some Error")
+	})
+
+	r.GET("expected", func(ctx *gin.Context) {
+		panic(NewException(fmt.Errorf("Expected wrapped error"), "Error Occured", "An expected error occured", "", "", 400, 10000))
 	})
 
 	r.Run()
